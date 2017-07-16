a4b855ff82eb700a508a92f66d3254a0767d136a
Neel_Patel <neel.patel.2012.np@gmail.com>
2017-07-16 22:44:27 +0530

    add some code for user authentication.

---
+++ src/java/com/articles_hub/service/AuthenticationService.java
@@ -0,0 +1,143 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Neel Patel.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.articles_hub.service;
+
+import com.articles_hub.database.DataBase;
+import com.articles_hub.database.beans.UserProfile;
+import com.articles_hub.database.beans.UserToken;
+import java.util.List;
+import javax.persistence.FlushModeType;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+//import org.hibernate.resource.transaction.spi.TransactionStatus;
+//import org.hibernate.query.Query;
+
+/**
+ *
+ * @author Neel Patel
+ */
+public class AuthenticationService {
+    
+    private static AuthenticationService obj;
+    
+    public static AuthenticationService getAuthenticationService(){
+        if(obj==null)
+        synchronized(AuthenticationService.class){
+            if(obj==null)
+                obj=new AuthenticationService();
+        }    
+        return obj;
+    }
+    
+    private DataBase db;
+    
+    private AuthenticationService(){
+        db=DataBase.getDataBase();
+//        System.err.println("authentication service initialized");
+    }
+    
+    public String userLogin(String userName,String pass){
+        String token = getToken(userName);
+        if(token!=null && !token.trim().equals(""))
+            return token;
+        Session session=db.getSession();
+        Transaction t=session.beginTransaction();
+        try{
+            Query q= session.getNamedQuery("UserProfile.byName");
+            q.setParameter("name", userName);
+            List<UserProfile> list = q.list();
+            if(list.size()!=1)
+                return null;
+            UserProfile user=list.get(0);
+            if(!user.getPass().equals(pass))
+                return null;
+            UserToken tokenOb=new UserToken(user);
+            session.save(tokenOb);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            if(t!=null&&t.isActive())
+                t.rollback();
+        }
+        return getToken(userName);
+    }
+    
+    public boolean userLogout(String token){
+        Session session=db.getSession();
+        Transaction t=session.beginTransaction();
+        try{
+            session.setFlushMode(FlushModeType.AUTO);
+            UserToken tokenObj=session.get(UserToken.class, token);
+            if(tokenObj==null)
+                return false;
+            session.delete(tokenObj);
+            return true;
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            if(t!=null&&t.isActive()&&!t.getRollbackOnly())
+                t.commit();
+        }
+        return false;
+    }
+    
+    private String getToken(String userName){
+        Session session=db.getSession();
+        Transaction t=session.beginTransaction();
+        try{
+            Query q= session.getNamedQuery("UserToken.byName");
+            q.setParameter("name", userName);
+            List<UserToken> list = q.list();
+            if(list.size()!=1)
+                return null;
+            list.get(0).getToken();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            if(t!=null&&t.isActive())
+                t.rollback();
+        }
+        return null;
+    }
+    
+    public String getUserName(String token){
+        Session session=db.getSession();
+        Transaction t=session.beginTransaction();
+        try{
+            session.setFlushMode(FlushModeType.AUTO);
+            UserToken tokenObj=session.get(UserToken.class, token);
+            if(tokenObj==null)
+                return null;
+            return tokenObj.getUser().getUserName();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            if(t!=null&&t.isActive()&&!t.getRollbackOnly())
+                t.commit();
+        }
+        return null;
+    }
+    
+}
